ec68984662448b0fa00b4986f4dd06b69a88e0a1
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-10-05 18:16:59 +0300

    UI enhancement to add loader on grids

--- src/components/Harvests/Harvests.tsx
+++ src/components/Harvests/Harvests.tsx
@@ -4,6 +4,8 @@ import utils from "../utils";
 import axios from "axios";
 import { useSnackbar } from "notistack";
 import AddHarvestModal from "./AddHarvestModal";
+import { AiOutlinePlus } from "react-icons/ai";
+import Loader from "../common/Loader";
 
 interface HarvestProps {
     harvestid: string;
@@ -24,9 +26,11 @@ const Harvests: React.FC = () => {
     const [selectedHarvest, setSelectedHarvest] = useState<HarvestProps | null>(null);
     const { enqueueSnackbar } = useSnackbar();
     const [harvests, setHarvests] = useState<HarvestProps[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const fetchData = async () => {
         try {
+            setIsLoading(true);
             const url = `${utils.baseUrl}/api/harvests/list`;
             const response = await axios.post(url, {
                 headers: { 'Content-Type': 'application/json' },
@@ -36,6 +40,8 @@ const Harvests: React.FC = () => {
             localStorage.setItem('harvests', JSON.stringify(harvestData));
         } catch (error) {
             enqueueSnackbar("Harvests Loading Failed. Please try again.", { variant: "error" });
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -49,53 +55,60 @@ const Harvests: React.FC = () => {
     };
 
     return (
-        <div className="container mx-auto px-4 py-6">
-            <div className="flex justify-between items-center mb-4">
-                <h1 className="text-2xl font-semibold">All Harvests</h1>
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                    onClick={() => {
-                        setSelectedHarvest(null);
-                        setHarvestShowModal(true);
-                    }}
-                >
-                    Add Harvest
-                </button>
-            </div>
+        <div className="container mx-auto px-2 py-6">
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <div className="flex justify-between items-center mb-4">
+                        <h1 className="text-2xl font-semibold">All Harvests</h1>
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 transition text-white px-4 py-2 rounded-lg shadow-md flex items-center"
+                            onClick={() => {
+                                setSelectedHarvest(null);
+                                setHarvestShowModal(true);
+                            }}
+                        >
+                            <AiOutlinePlus className="text-lg md:mr-1" />
+                            <span className="hidden md:inline text-sm">Add Harvest</span>
+                        </button>
+                    </div>
 
-            <div className="overflow-x-auto">
-                <table className="min-w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-collapse">
-                    <thead>
-                        <tr className="border-b dark:border-gray-700">
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">BAGS</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">UNIT PRICE</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">AMOUNT SOLD</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[300px]">NOTES</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[250px]">CREATED ON</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[250px]">MODIFIED ON</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">CREATED BY</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">MODIFIED BY</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">ACTIONS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {harvests && harvests.map((harvest) => (
-                            <HarvestRow
-                                key={harvest.harvestid}
-                                harvest={harvest}
-                                onEdit={() => handleEditHarvest(harvest)}
-                            />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-collapse">
+                            <thead>
+                                <tr className="border-b dark:border-gray-700">
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">BAGS</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">UNIT PRICE</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">AMOUNT SOLD</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[300px]">NOTES</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[250px]">CREATED ON</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[250px]">MODIFIED ON</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">CREATED BY</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">MODIFIED BY</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold min-w-[150px]">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {harvests && harvests.map((harvest) => (
+                                    <HarvestRow
+                                        key={harvest.harvestid}
+                                        harvest={harvest}
+                                        onEdit={() => handleEditHarvest(harvest)}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
 
-            {showHarvestModal && (
-                <AddHarvestModal
-                    showHarvestModal={showHarvestModal}
-                    setHarvestShowModal={setHarvestShowModal}
-                    harvest={selectedHarvest}
-                />
+                    {showHarvestModal && (
+                        <AddHarvestModal
+                            showHarvestModal={showHarvestModal}
+                            setHarvestShowModal={setHarvestShowModal}
+                            harvest={selectedHarvest}
+                        />
+                    )}
+                </>
             )}
         </div>
     );
--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useSnackbar } from "notistack";
 import TransactionRow from "./TransactionRow";
 import AddTransactionModal from "./AddTransactionModal";
+import { AiOutlineDownload, AiOutlinePlus, AiOutlineUpload } from "react-icons/ai";
+import Loader from "../common/Loader";
 
 interface TransactionProps {
     name: string;
@@ -16,9 +18,11 @@ const Transactions: React.FC = () => {
     const [showTransactionModal, setTransactionShowModal] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
     const [transactions, setTransactions] = useState<TransactionProps[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const fetchData = async () => {
         try {
+            setIsLoading(true);
             const url = `${utils.baseUrl}/api/transactions/list`;
             const response = await axios.post(url, {
                 headers: { 'Content-Type': 'application/json' },
@@ -28,6 +32,8 @@ const Transactions: React.FC = () => {
             localStorage.setItem('transactions', JSON.stringify(transactionData));
         } catch (error) {
             enqueueSnackbar("Transactions Loading Failed. Please try again.", { variant: "error" });
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -63,7 +69,6 @@ const Transactions: React.FC = () => {
         const fileInput = document.createElement('input');
         fileInput.type = 'file';
         fileInput.accept = '.csv, .xlsx';
-
         fileInput.onchange = (e: Event) => {
             const target = e.target as HTMLInputElement;
             const file = target.files ? target.files[0] : null;
@@ -75,69 +80,78 @@ const Transactions: React.FC = () => {
     };
 
     return (
-        <div className="container mx-auto px-4 py-6">
-            <div className="flex flex-col md:flex-row items-center justify-between w-full mb-4 space-y-4 md:space-y-0">
-                <h1 className="text-2xl font-semibold w-full md:w-auto text-center md:text-left">All Transactions</h1>
-                <div className="flex flex-col md:flex-row items-center w-full md:w-auto md:space-x-4 space-y-4 md:space-y-0">
-                    <button
-                        className="bg-blue-600 hover:bg-blue-700 transition text-white px-6 py-2 rounded-lg w-full md:w-auto shadow-md"
-                        onClick={() => setTransactionShowModal(true)}
-                    >
-                        Add Transaction
-                    </button>
-                    <button
-                        className="bg-green-600 hover:bg-green-700 transition text-white px-6 py-2 rounded-lg w-full md:w-auto shadow-md"
-                        onClick={handleImportTransactions}
-                    >
-                        Import Transactions
-                    </button>
-                    <button
-                        className="bg-gray-300 hover:bg-gray-400 transition text-black px-6 py-2 rounded-lg w-full md:w-auto shadow-md"
-                    >
-                        Export
-                    </button>
-                </div>
-            </div>
+        <div className="container mx-auto px-2 py-6">
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <div className="flex flex-row items-center justify-between w-full mb-4 space-x-2">
+                        <h1 className="text-2xl font-semibold w-full md:w-auto text-center md:text-left">All Transactions</h1>
+                        <div className="flex flex-row items-center w-full md:w-auto space-x-2">
+                            <button
+                                className="bg-blue-600 hover:bg-blue-700 transition text-white px-3 py-2 rounded-lg shadow-md flex items-center"
+                                onClick={() => setTransactionShowModal(true)}
+                            >
+                                <AiOutlinePlus className="text-lg md:mr-1" />
+                                <span className="hidden md:inline text-sm">Add Transaction</span>
+                            </button>
+                            <button
+                                className="bg-green-600 hover:bg-green-700 transition text-white px-3 py-2 rounded-lg shadow-md flex items-center"
+                                onClick={handleImportTransactions}
+                            >
+                                <AiOutlineUpload className="text-lg md:mr-1" />
+                                <span className="hidden md:inline text-sm">Import Transactions</span>
+                            </button>
+                            <button
+                                className="bg-gray-300 hover:bg-gray-400 transition text-black px-3 py-2 rounded-lg shadow-md flex items-center"
+                            >
+                                <AiOutlineDownload className="text-lg md:mr-1" />
+                                <span className="hidden md:inline text-sm">Export</span>
+                            </button>
+                        </div>
+                    </div>
 
-            <div className="relative mb-4">
-                <input
-                    type="text"
-                    placeholder="Search for transactions"
-                    className="w-full px-4 py-2 border rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200"
-                />
-            </div>
+                    <div className="relative mb-4">
+                        <input
+                            type="text"
+                            placeholder="Search for transactions"
+                            className="w-full px-4 py-2 border rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200"
+                        />
+                    </div>
 
-            <div className="overflow-x-auto">
-                <table className="min-w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-collapse">
-                    <thead>
-                        <tr className="border-b dark:border-gray-700">
-                            <th className="px-4 py-2 text-left text-sm font-semiboldmin-w-[200px]">NAME</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold">AMOUNT</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold">PAIDBY</th>
-                            <th className="px-4 py-2 text-left text-sm font-semiboldmin-w-[250px]">NOTES</th>
-                            <th className="px-4 py-2 text-left text-sm font-semibold">ACTIONS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {transactions &&
-                            transactions.map((transaction, index) => (
-                                <TransactionRow
-                                    key={index}
-                                    name={transaction.name}
-                                    amount={transaction.amount}
-                                    notes={transaction.notes}
-                                    paidby={transaction.clientusername}
-                                />
-                            ))}
-                    </tbody>
-                </table>
-                {showTransactionModal && (
-                    <AddTransactionModal
-                        showTransactionModal={showTransactionModal}
-                        setTransactionShowModal={setTransactionShowModal}
-                    />
-                )}
-            </div>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-collapse">
+                            <thead>
+                                <tr className="border-b dark:border-gray-700">
+                                    <th className="px-4 py-2 text-left text-sm font-semiboldmin-w-[200px]">NAME</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold">AMOUNT</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold">PAIDBY</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semiboldmin-w-[250px]">NOTES</th>
+                                    <th className="px-4 py-2 text-left text-sm font-semibold">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {transactions &&
+                                    transactions.map((transaction, index) => (
+                                        <TransactionRow
+                                            key={index}
+                                            name={transaction.name}
+                                            amount={transaction.amount}
+                                            notes={transaction.notes}
+                                            paidby={transaction.clientusername}
+                                        />
+                                    ))}
+                            </tbody>
+                        </table>
+                        {showTransactionModal && (
+                            <AddTransactionModal
+                                showTransactionModal={showTransactionModal}
+                                setTransactionShowModal={setTransactionShowModal}
+                            />
+                        )}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
