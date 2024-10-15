0fe3de866d023e08baebcac35bd16c628308b6f5
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-10-16 00:40:19 +0300

    update corousel buttons ui

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -41,14 +41,16 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
             {/* Next and Previous buttons */}
             <button
                 onClick={prevSlide}
-                className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-3 rounded-full z-10"
+                className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-300 dark:bg-gray-800 text-gray-800 dark:text-white flex items-center justify-center rounded-full z-10 hover:bg-gray-400 dark:hover:bg-gray-700"
+                style={{ width: "40px", height: "40px", fontSize: "20px" }}
                 aria-label="Previous Slide"
             >
                 &#10094;
             </button>
             <button
                 onClick={nextSlide}
-                className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-800 text-white p-3 rounded-full z-10"
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-300 dark:bg-gray-800 text-gray-800 dark:text-white flex items-center justify-center rounded-full z-10 hover:bg-gray-400 dark:hover:bg-gray-700"
+                style={{ width: "40px", height: "40px", fontSize: "20px" }}
                 aria-label="Next Slide"
             >
                 &#10095;
@@ -61,8 +63,8 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
                         key={index}
                         onClick={() => setCurrentIndex(index)}
                         className={`w-3 h-3 rounded-full ${currentIndex === index
-                            ? "bg-gray-800"
-                            : "bg-gray-400 hover:bg-gray-600"
+                            ? "bg-gray-800 dark:bg-white"
+                            : "bg-gray-400 dark:bg-gray-600 hover:bg-gray-500 dark:hover:bg-gray-400"
                             }`}
                         aria-label={`Go to slide ${index}`}
                     />
