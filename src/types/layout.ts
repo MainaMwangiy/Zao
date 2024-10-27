5bdc1e76ee9ef289645e498ace895ac6e8ba1eb0
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-10-27 20:42:38 +0300

    #86 - remove projects for other prganizations & only show in project level

--- src/types/layout.ts
+++ src/types/layout.ts
@@ -1,54 +1,57 @@
 export interface ProjectsProps {
-    name: string;
-    location: string;
-    size: string;
-    status: string;
-    projectplan: boolean;
-    imagesurl: string;
-    projectid: string;
-    roleid: number;
-    costprojectestimation: string;
-  }
-  
-  export  interface Organization {
-    clientorganizationid: number;
-    name: string;
-    appconfig: {
-      dateFormat?: string;
-    };
-    createdon: string;
-    createdbyuserid: number;
-    modifiedon: string;
-    modifiedbyuserid: number;
-    isdeleted: number;
-  }
-  
-  export type BlobItem = {
-    url: string;
-    downloadUrl: string;
-    pathname: string;
-    size: number;
-    uploadedAt: string;
-  };
-  
-  export  type BlobsData = {
-    hasMore: boolean;
-    data: BlobItem[];
-  };
-  
-  export  type ListResponse = {
-    success: boolean;
-    data: BlobsData;
-  };
+  name: string;
+  location: string;
+  size: string;
+  status: string;
+  projectplan: boolean;
+  imagesurl: string;
+  projectid: string;
+  roleid: number;
+  costprojectestimation: string;
+}
 
-  export  interface SidebarProps {
-    isOpen?: boolean,
-    toggleSidebar?: () => void;
-  }
-  
-  export interface SidebarProps {
-    isMobile?: boolean;
-    isOpen?: boolean;
-    closeSidebar?: () => void;
-  }
-  
+export type ClientConfig = {
+  dateFormat?: string;
+  showGallery?: boolean;
+  showTransactions?: boolean;
+};
+
+export interface Organization {
+  clientorganizationid: number;
+  name: string;
+  appconfig: ClientConfig;
+  createdon: string;
+  createdbyuserid: number;
+  modifiedon: string;
+  modifiedbyuserid: number;
+  isdeleted: number;
+}
+
+export type BlobItem = {
+  url: string;
+  downloadUrl: string;
+  pathname: string;
+  size: number;
+  uploadedAt: string;
+};
+
+export type BlobsData = {
+  hasMore: boolean;
+  data: BlobItem[];
+};
+
+export type ListResponse = {
+  success: boolean;
+  data: BlobsData;
+};
+
+export interface SidebarProps {
+  isOpen?: boolean,
+  toggleSidebar?: () => void;
+}
+
+export interface SidebarProps {
+  isMobile?: boolean;
+  isOpen?: boolean;
+  closeSidebar?: () => void;
+}
