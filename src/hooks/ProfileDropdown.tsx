84e91c92a23a6c3d06bd6f6003dca4305cd06325
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-12-05 23:06:02 +0300

    show image from single sign on

--- src/hooks/ProfileDropdown.tsx
+++ src/hooks/ProfileDropdown.tsx
@@ -5,12 +5,14 @@ import { Link, useNavigate } from 'react-router-dom';
 interface User {
   name: string;
   email: string;
+  image_url: string;
 }
 
 const ProfileDropdown: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [loggedUser, setLoggedUser] = useState<User | null>(null);
   const navigate = useNavigate();
+  const { image_url } = loggedUser || {}
 
   const getStoredUser = (): User | null => {
     const storedUser = localStorage.getItem('clientuser');
@@ -65,15 +67,25 @@ const ProfileDropdown: React.FC = () => {
 
   return (
     <div className="menu relative bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
-        <FaUserCircle className="text-gray-500 cursor-pointer" size={32} />
-      </button>
+      <div className="flex items-center space-x-6" onClick={() => setIsOpen(!isOpen)}>
+        {image_url ? (
+          <img src={image_url} alt="Profile" className="w-12 h-12 rounded-full" />
+        ) : (
+          <FaUserCircle className="text-gray-500 cursor-pointer" size={32} />
+        )}
+      </div>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-50">
+        <div className="absolute right-0 mt-2 w-72 bg-white shadow-lg rounded-lg z-50">
           {/* User Info Section */}
           <div className="flex items-center px-2 py-2 border-b border-gray-200">
-            <FaUserCircle className="text-gray-500 flex-shrink-0" size={30} />
-            <div className="ml-3 flex-grow">
+            <div className="flex-shrink-0 mr-3">
+              {image_url ? (
+                <img src={image_url} alt="Profile" className="w-12 h-12 rounded-full" />
+              ) : (
+                <FaUserCircle className="text-gray-500" size={30} />
+              )}
+            </div>
+            <div className="flex flex-col ml-3 flex-grow">
               <p className="text-gray-800 font-semibold truncate">{loggedUser?.name || "Guest"}</p>
               <p className="text-sm text-gray-500 truncate">{loggedUser?.email || "No email available"}</p>
             </div>
